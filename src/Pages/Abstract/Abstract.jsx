d8698e7c89bcefbc04185898f8f8ff7c95c4f8a6
foxstudent107933 <tulupaanton11@gmail.com>
2025-09-09 00:25:50 +0300

    adding mapping for json array

--- src/Pages/Abstract/Abstract.jsx
+++ src/Pages/Abstract/Abstract.jsx
@@ -1,174 +1,30 @@
+import AbstractsData from '../../data/abstracts.json'
+
 export function Abstract () {
   return (
-    <div className='flex flex-col max-w-screen-xl mx-auto px-4 border-4 border-[#8b5e3c] rounded-2xl shadow-2xl m-5 bg-[#fdfaf6]'>
-      {/* Sidebar */}
-      <aside className=' flex flex-col w-72 p-4 border-r-4 border-[#8b5e3c] bg-[#f8f1e7]'>
-        <input
-          type='text'
-          placeholder='Пошук'
-          className='uk-input mb-4 rounded-lg'
-        />
-        <ul className='uk-nav uk-nav-default'>
-          <li className='uk-active'>
-            <a href='#'>Вступ</a>
-          </li>
-          <li>
-            <a href='#'>Київська Русь</a>
-          </li>
-          <li>
-            <a href='#'>Козацька доба</a>
-          </li>
-          <li>
-            <a href='#'>Гетьманщина</a>
-          </li>
-          <li>
-            <a href='#'>Новий час</a>
-          </li>
-          <li>
-            <a href='#'>ХХ століття</a>
-          </li>
-          <li>
-            <a href='#'>Незалежність</a>
-          </li>
-        </ul>
-      </aside>
-
-      {/* Content */}
-      <section className='flex-1 p-6'>
-        {/* <h2 className='text-2xl font-bold text-[#5a3e2b] mb-4'>
-          Київська Русь (IX–XIII ст.)
-        </h2> */}
-
-        {/* Карточка конспекта */}
-        <div className='bg-white border-2 border-[#8b5e3c] rounded-xl p-6 shadow-md mb-6'>
-          <p className='mb-4 text-lg leading-relaxed text-[#2c2c2c]'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
-            voluptas illum vitae, dignissimos quos laborum nisi at amet saepe
-            minima expedita, alias eveniet animi excepturi accusantium optio ad
-            reiciendis facilis.
-          </p>
-
-          {/* Цитата */}
-          <blockquote className='border-l-4 border-[#d4af37] pl-4 italic text-[#5a3e2b] mb-4'>
-            lorem
-          </blockquote>
-
-          {/* Картинка */}
-          <img
-            src='/images/kyiv_rus_map.jpg'
-            alt='lorem'
-            className='rounded-lg shadow-md'
-          />
-        </div>
-      </section>
-      <section className='flex-1 p-6'>
-        {/* <h2 className='text-2xl font-bold text-[#5a3e2b] mb-4'>
-          Київська Русь (IX–XIII ст.)
-        </h2> */}
-
-        {/* Карточка конспекта */}
-        <div className='bg-white border-2 border-[#8b5e3c] rounded-xl p-6 shadow-md mb-6'>
-          <p className='mb-4 text-lg leading-relaxed text-[#2c2c2c]'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
-            voluptas illum vitae, dignissimos quos laborum nisi at amet saepe
-            minima expedita, alias eveniet animi excepturi accusantium optio ad
-            reiciendis facilis.
-          </p>
-
-          {/* Цитата */}
-          <blockquote className='border-l-4 border-[#d4af37] pl-4 italic text-[#5a3e2b] mb-4'>
-            lorem
-          </blockquote>
-
-          {/* Картинка */}
-          <img
-            src='/images/kyiv_rus_map.jpg'
-            alt='lorem'
-            className='rounded-lg shadow-md'
-          />
-        </div>
-      </section>
-      <section className='flex-1 p-6'>
-        {/* <h2 className='text-2xl font-bold text-[#5a3e2b] mb-4'>
-          Київська Русь (IX–XIII ст.)
-        </h2> */}
-
-        {/* Карточка конспекта */}
-        <div className='bg-white border-2 border-[#8b5e3c] rounded-xl p-6 shadow-md mb-6'>
-          <p className='mb-4 text-lg leading-relaxed text-[#2c2c2c]'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
-            voluptas illum vitae, dignissimos quos laborum nisi at amet saepe
-            minima expedita, alias eveniet animi excepturi accusantium optio ad
-            reiciendis facilis.
-          </p>
-
-          {/* Цитата */}
-          <blockquote className='border-l-4 border-[#d4af37] pl-4 italic text-[#5a3e2b] mb-4'>
-            lorem
-          </blockquote>
-
-          {/* Картинка */}
-          <img
-            src='/images/kyiv_rus_map.jpg'
-            alt='lorem'
-            className='rounded-lg shadow-md'
-          />
-        </div>
-      </section>
-      <section className='flex-1 p-6'>
-        {/* <h2 className='text-2xl font-bold text-[#5a3e2b] mb-4'>
-          Київська Русь (IX–XIII ст.)
-        </h2> */}
-
-        {/* Карточка конспекта */}
-        <div className='bg-white border-2 border-[#8b5e3c] rounded-xl p-6 shadow-md mb-6'>
-          <p className='mb-4 text-lg leading-relaxed text-[#2c2c2c]'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
-            voluptas illum vitae, dignissimos quos laborum nisi at amet saepe
-            minima expedita, alias eveniet animi excepturi accusantium optio ad
-            reiciendis facilis.
-          </p>
-
-          {/* Цитата */}
-          <blockquote className='border-l-4 border-[#d4af37] pl-4 italic text-[#5a3e2b] mb-4'>
-            lorem
-          </blockquote>
-
-          {/* Картинка */}
-          <img
-            src='/images/kyiv_rus_map.jpg'
-            alt='lorem'
-            className='rounded-lg shadow-md'
-          />
-        </div>
-      </section>
-      <section className='flex-1 p-6'>
-        {/* <h2 className='text-2xl font-bold text-[#5a3e2b] mb-4'>
-          Київська Русь (IX–XIII ст.)
-        </h2> */}
-
-        {/* Карточка конспекта */}
-        <div className='bg-white border-2 border-[#8b5e3c] rounded-xl p-6 shadow-md mb-6'>
-          <p className='mb-4 text-lg leading-relaxed text-[#2c2c2c]'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
-            voluptas illum vitae, dignissimos quos laborum nisi at amet saepe
-            minima expedita, alias eveniet animi excepturi accusantium optio ad
-            reiciendis facilis.
-          </p>
-
-          {/* Цитата */}
-          <blockquote className='border-l-4 border-[#d4af37] pl-4 italic text-[#5a3e2b] mb-4'>
-            lorem
-          </blockquote>
-
-          {/* Картинка */}
-          <img
-            src='/images/kyiv_rus_map.jpg'
-            alt='lorem'
-            className='rounded-lg shadow-md'
-          />
-        </div>
-      </section>
+    <div className='flex flex-col max-w-screen-xl mx-auto m-5 bg-[#fdfaf6] p-5 rounded-lg'>
+      {/* CARDS */}
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
+        {AbstractsData.map(note => (
+          <div
+            key={note.id}
+            className='bg-white rounded-xl shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300'
+          >
+            <img
+              src={note.image}
+              alt={note.title}
+              className='w-full h-48 object-cover'
+            />
+            <div className='p-4'>
+              <h2 className='text-xl font-semibold mb-2'>{note.title}</h2>
+              <p className='text-gray-600 mb-3'>{note.description}</p>
+              <span className='text-sm text-gray-500 font-medium'>
+                {note.date}
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
