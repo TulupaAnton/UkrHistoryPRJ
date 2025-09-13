ced1224645747f4e647532fdaad70ed55e4ccb9d
foxstudent107933 <tulupaanton11@gmail.com>
2025-09-13 22:11:53 +0300

    adding a new components

--- src/Pages/AncientHistoryPage/AncientHistoryPage.jsx
+++ src/Pages/AncientHistoryPage/AncientHistoryPage.jsx
@@ -106,6 +106,7 @@ export function AncientHistoryPage () {
             </div>
           ))}
         </div>
+        <div class='absolute inset-x-0 bottom-0 h-px bg-gradient-to-r from-transparent via-amber-500 to-transparent'></div>
       </div>
 
       {/* Stone Age Section */}
@@ -124,10 +125,10 @@ export function AncientHistoryPage () {
       </div>
 
       {/* Stone Age Sub-Periods */}
-      <div className='w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-16 relative z-10'>
+      <div className='flex flex-col w-full p-5 flex-wrap'>
         <div className='flex flex-col gap-8'>
           {/* Paleolithic */}
-          <div className='bg-gradient-to-br from-gray-800 to-gray-900 rounded-2xl shadow-2xl border-t-4 border-blue-500 overflow-hidden transition-transform duration-300 hover:scale-[1.01]'>
+          <div className='font-six bg-gradient-to-br from-gray-800 to-gray-900 rounded-2xl shadow-2xl border-t-4 border-blue-500 overflow-hidden'>
             <div className='bg-gradient-to-r from-blue-600 to-blue-700 py-4 text-center'>
               <h3 className='text-2xl font-semibold text-white'>Палеоліт</h3>
               <p className='text-lg text-blue-100 mt-1'>
@@ -327,7 +328,7 @@ export function AncientHistoryPage () {
           </div>
 
           {/* Neolithic */}
-          <div className='bg-gradient-to-br from-gray-800 to-gray-900 rounded-2xl shadow-2xl border-t-4 border-purple-500 overflow-hidden transition-transform duration-300 hover:scale-[1.01]'>
+          <div className='bg-gradient-to-br from-gray-800 to-gray-900 rounded-2xl shadow-2xl border-t-4 border-purple-500 overflow-hidden'>
             <div className='bg-gradient-to-r from-purple-600 to-purple-700 py-4 text-center'>
               <h3 className='text-2xl font-semibold text-white'>Неоліт</h3>
               <p className='text-lg text-purple-100 mt-1'>
@@ -515,12 +516,240 @@ export function AncientHistoryPage () {
           </div>
 
           {/* Декоративный элемент внизу */}
-          <div className='text-center mt-20 pt-8 border-t border-gray-700/50 relative'>
+          {/* <div className='text-center mt-20 pt-8 border-t border-gray-700/50 relative'>
             <div className='absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-16 h-16 bg-amber-500/10 rounded-full flex items-center justify-center'>
               <i className='fas fa-monument text-amber-400/50 text-2xl'></i>
             </div>
             <p className='text-gray-400 italic'>
               Археологічні пам'ятки давньої історії України
+            </p>
+          </div> */}
+        </div>
+      </div>
+      {/*//! Enolit */}
+      <div className='bg-gray-800/50 rounded-2xl p-8 shadow-2xl border border-amber-700/30 relative overflow-hidden'>
+        <div className='absolute top-0 right-0 w-32 h-32 bg-amber-500/5 rounded-full -translate-y-16 translate-x-16'></div>
+        <div className='absolute bottom-0 left-0 w-40 h-40 bg-amber-400/5 rounded-full translate-y-20 -translate-x-20'></div>
+
+        <div className='flex items-center text-center justify-center mb-8'>
+          <h1 className='font-bold text-5xl md:text-6xl mb-6 bg-clip-text text-transparent bg-gradient-to-r from-amber-300 via-amber-400 to-amber-500 relative inline-block'>
+            <span className='absolute -inset-4 bg-amber-400 blur-xl opacity-20 rounded-full'></span>
+            Енеоліт, або мідно-кам'яний вік <br />
+            (4-3 тис. до н.е.)
+          </h1>
+        </div>
+
+        <div className='space-y-4 relative z-10'>
+          <div className='flex items-start p-5 rounded-xl bg-gradient-to-r from-gray-700/40 to-gray-800/40 hover:from-amber-900/30 hover:to-amber-800/30 transition-all duration-300 group border border-gray-700/50 hover:border-amber-700/30'>
+            <span className='inline-flex items-center justify-center w-8 h-8 bg-gradient-to-br from-amber-500 to-amber-700 text-gray-900 rounded-lg text-sm font-bold mr-4 flex-shrink-0 shadow-md'>
+              1
+            </span>
+            <span className='text-lg md:text-xl'>
+              Початок обробки металів (міді та золота) зі збереженням значної
+              ролі каменю
+            </span>
+          </div>
+
+          <div className='flex items-start p-5 rounded-xl bg-gradient-to-r from-gray-700/40 to-gray-800/40 hover:from-amber-900/30 hover:to-amber-800/30 transition-all duration-300 group border border-gray-700/50 hover:border-amber-700/30'>
+            <span className='inline-flex items-center justify-center w-8 h-8 bg-gradient-to-br from-amber-500 to-amber-700 text-gray-900 rounded-lg text-sm font-bold mr-4 flex-shrink-0 shadow-md'>
+              2
+            </span>
+            <span className='text-lg md:text-xl'>
+              Скотарство відокремлюється від хліборобства. З'являються пастуші
+              племена. Перехід від мотичного до рільного землеробства
+            </span>
+          </div>
+
+          <div className='flex items-start p-5 rounded-xl bg-gradient-to-r from-gray-700/40 to-gray-800/40 hover:from-amber-900/30 hover:to-amber-800/30 transition-all duration-300 group border border-gray-700/50 hover:border-amber-700/30'>
+            <span className='inline-flex items-center justify-center w-8 h-8 bg-gradient-to-br from-amber-500 to-amber-700 text-gray-900 rounded-lg text-sm font-bold mr-4 flex-shrink-0 shadow-md'>
+              3
+            </span>
+            <span className='text-lg md:text-xl'>
+              Винайдені суцільні колесо й чотириколісні вози, що сприяло
+              розширенню контактів і розселенню людей
+            </span>
+          </div>
+
+          <div className='flex items-start p-5 rounded-xl bg-gradient-to-r from-gray-700/40 to-gray-800/40 hover:from-amber-900/30 hover:to-amber-800/30 transition-all duration-300 group border border-gray-700/50 hover:border-amber-700/30'>
+            <span className='inline-flex items-center justify-center w-8 h-8 bg-gradient-to-br from-amber-500 to-amber-700 text-gray-900 rounded-lg text-sm font-bold mr-4 flex-shrink-0 shadow-md'>
+              4
+            </span>
+            <span className='text-lg md:text-xl'>
+              Починається перехід від родової до{' '}
+              <span className='font-bold text-amber-300'>
+                сусідської общини
+              </span>
+            </span>
+          </div>
+          <div className='flex items-start p-5 rounded-xl bg-gradient-to-r from-gray-700/40 to-gray-800/40 hover:from-amber-900/30 hover:to-amber-800/30 transition-all duration-300 group border border-gray-700/50 hover:border-amber-700/30'>
+            <span className='inline-flex items-center justify-center w-8 h-8 bg-gradient-to-br from-amber-500 to-amber-700 text-gray-900 rounded-lg text-sm font-bold mr-4 flex-shrink-0 shadow-md'>
+              5
+            </span>
+            <span className='text-lg md:text-xl'>
+              Скотарство відокремлюється від хліборобства. З'являються пастуші
+              племена. Перехід від мотичного до рільного землеробства
+            </span>
+          </div>
+          <div className='flex items-start p-5 rounded-xl bg-gradient-to-r from-gray-700/40 to-gray-800/40 hover:from-amber-900/30 hover:to-amber-800/30 transition-all duration-300 group border border-gray-700/50 hover:border-amber-700/30'>
+            <span className='inline-flex items-center justify-center w-8 h-8 bg-gradient-to-br from-amber-500 to-amber-700 text-gray-900 rounded-lg text-sm font-bold mr-4 flex-shrink-0 shadow-md'>
+              6
+            </span>
+            <span className='text-lg md:text-xl'>
+              Стався перший{' '}
+              <span className='font-bold text-amber-300'>
+                суспільний поділ праці
+              </span>
+              - скотарство відокремлюється від землеробства
+            </span>
+            <span className='font-bold text-amber-300'>!!!!!!!!!!</span>
+          </div>
+          <div className='flex items-start p-5 rounded-xl bg-gradient-to-r from-gray-700/40 to-gray-800/40 hover:from-amber-900/30 hover:to-amber-800/30 transition-all duration-300 group border border-gray-700/50 hover:border-amber-700/30'>
+            <span className='inline-flex items-center justify-center w-8 h-8 bg-gradient-to-br from-amber-500 to-amber-700 text-gray-900 rounded-lg text-sm font-bold mr-4 flex-shrink-0 shadow-md'>
+              7
+            </span>
+            <span className='text-lg md:text-xl'>Перші транспорті засоби</span>
+          </div>
+        </div>
+      </div>
+
+      {/* //! Археологічні культури  */}
+      <div className='bg-gray-800/50 rounded-2xl p-8 shadow-2xl border mt-5 border-amber-700/30 relative overflow-hidden'>
+        <div className='flex items-center text-center justify-center mb-8'>
+          <h1 className='font-bold text-5xl md:text-6xl mb-6 bg-clip-text text-transparent bg-gradient-to-r from-amber-300 via-amber-400 to-amber-500 relative inline-block'>
+            <span className='absolute -inset-4 bg-amber-400 blur-xl opacity-20 rounded-full'></span>
+            Археологічні культури
+          </h1>
+        </div>
+        <div>
+          <p className='text-2xl'>
+            <span className='text-3xl font-bold text-amber-300'>
+              Археологічна культура
+            </span>{' '}
+            - група подібних між собою археологічних пам'яток (за формами та
+            оздобленням посуду, прикрасами,поховальними звичаями тощо),
+            поширених на певній території в межах певного історичного часу.
+          </p>
+        </div>
+      </div>
+      {/* //! ТРИПІЛЬЦІ */}
+      <div className='bg-gray-800/50 rounded-2xl mt-5 p-8 shadow-2xl border border-amber-700/30 relative overflow-hidden'>
+        <div className='absolute top-0 right-0 w-32 h-32 bg-amber-500/5 rounded-full -translate-y-16 translate-x-16'></div>
+        <div className='absolute bottom-0 left-0 w-40 h-40 bg-amber-400/5 rounded-full translate-y-20 -translate-x-20'></div>
+
+        <div className='flex items-center text-center justify-center mb-8'>
+          <h1 className='font-bold text-5xl md:text-6xl mb-6 bg-clip-text text-transparent bg-gradient-to-r from-amber-300 via-amber-400 to-amber-500 relative inline-block'>
+            <span className='absolute -inset-4 bg-amber-400 blur-xl opacity-20 rounded-full'></span>
+            Трипільська культура
+          </h1>
+        </div>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mb-8'>
+          {/* Хронологічні межі */}
+          <div className='flex items-start p-5 rounded-xl bg-gradient-to-r from-gray-700/40 to-gray-800/40 border border-gray-700/50'>
+            <div className='w-10 h-10 bg-gradient-to-br from-amber-500 to-amber-700 rounded-lg flex items-center justify-center mr-4 flex-shrink-0'>
+              <i className='fas fa-clock text-gray-900 text-sm'></i>
+            </div>
+            <div>
+              <p className='text-lg font-semibold text-amber-300 mb-2'>
+                Хронологічні межі
+              </p>
+              <p className='text-xl text-amber-400'>IV-III тис. до н.е.</p>
+            </div>
+          </div>
+
+          {/* Територія */}
+          <div className='flex items-start p-5 rounded-xl bg-gradient-to-r from-gray-700/40 to-gray-800/40 border border-gray-700/50'>
+            <div className='w-10 h-10 bg-gradient-to-br from-amber-500 to-amber-700 rounded-lg flex items-center justify-center mr-4 flex-shrink-0'>
+              <i className='fas fa-map-marker-alt text-gray-900 text-sm'></i>
+            </div>
+            <div>
+              <p className='text-lg font-semibold text-amber-300 mb-2'>
+                Територія
+              </p>
+              <p className='text-lg text-gray-300'>
+                Правобережжя України, Середнє Подніпров'я
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Господарство */}
+        <div className='bg-gradient-to-r from-gray-700/40 to-gray-800/40 rounded-xl p-6 border border-gray-700/50'>
+          <div className='flex items-center mb-6'>
+            <div className='w-10 h-10 bg-gradient-to-br from-amber-500 to-amber-700 rounded-lg flex items-center justify-center mr-4'>
+              <i className='fas fa-industry text-gray-900 text-sm'></i>
+            </div>
+            <h2 className='text-2xl font-semibold text-amber-400'>
+              Господарство
+            </h2>
+          </div>
+
+          <div className='space-y-4'>
+            <div className='flex items-start'>
+              <span className='w-6 h-6 bg-amber-600 rounded-full flex items-center justify-center text-gray-900 font-bold mr-3 mt-1 text-sm'>
+                1
+              </span>
+              <p className='text-lg text-gray-300'>
+                З'явилася перша мідна металургія, проте переважали кам'яні
+                знаряддя праці.
+              </p>
+            </div>
+
+            <div className='flex items-start'>
+              <span className='w-6 h-6 bg-amber-600 rounded-full flex items-center justify-center text-gray-900 font-bold mr-3 mt-1 text-sm'>
+                2
+              </span>
+              <p className='text-lg text-gray-300'>
+                Виготовляли досконалий глиняний посуд (трипільска кераміка),
+                який прикрашали орнаментами.
+              </p>
+            </div>
+
+            <div className='bg-amber-900/30 p-4 rounded-lg border-l-4 border-amber-500 mt-6'>
+              <p className='text-lg text-amber-300 mb-2'>
+                <span className='font-bold'>Екстенсивний спосіб життя</span> -
+                невміння відновлювати родючисть грунту - переселяються кожні 50
+                років
+              </p>
+            </div>
+
+            <div className='flex items-start mt-6'>
+              <span className='w-6 h-6 bg-amber-600 rounded-full flex items-center justify-center text-gray-900 font-bold mr-3 mt-1 text-sm'>
+                3
+              </span>
+              <p className='text-lg text-gray-300'>
+                <span className='font-semibold text-amber-300'>
+                  Протоміста!
+                </span>{' '}
+                переважно двоповерхові житла
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Место для изображений */}
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-8'>
+          <div className='bg-gray-700/50 rounded-xl p-4 text-center'>
+            <img src='./assets/vase.jpg' alt='Трипільська кераміка' />
+            <p className='text-gray-400 text-sm'>Трипільська кераміка</p>
+          </div>
+
+          <div className='bg-gray-700/50 rounded-xl p-4 text-center'>
+            <img src='./assets/protomisto.avif' alt='Трипільські протоміста' />
+            <p className='text-gray-400 text-sm'>Трипільські протоміста</p>
+          </div>
+          <div className='bg-gray-700/50 w-full rounded-xl p-4 text-center'>
+            <img src='./assets/karta_trip.jpg' alt='Карта' />
+            <p className='text-gray-400 text-sm'>Карта розселення трипільців</p>
+          </div>
+
+          <div className='bg-gray-700/50 w-full rounded-xl p-4 text-center'>
+            <img
+              src='./assets/unnamed.gif'
+              alt='   Глиняна моделька трипільського храму/будиночка'
+            />
+            <p className='text-gray-400 text-sm'>
+              Глиняна моделька трипільського храму/будиночка
             </p>
           </div>
         </div>
